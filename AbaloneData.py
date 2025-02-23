06f29983ac396aad7060462eb85e609e32cb52e6
Ro Nanak Prasad Lacoul <nanaklacoulbus@gmail.com>
2025-02-23 17:11:07 +0100

    tilføjet mere data analyse

--- AbaloneData.py
+++ AbaloneData.py
@@ -44,3 +44,41 @@ comparison = pd.DataFrame({
 })
 
 print("Comparison of mean values:\n", comparison)
+
+#male correlation
+correlation_matrix = D_male.drop(columns=['Sex']).corr()
+
+# Print the correlation matrix
+print("Correlation matrix for male abalones:\n", correlation_matrix)
+
+#female correlation
+correlation_matrix = D_female.drop(columns=['Sex']).corr()
+
+# Print the correlation matrix
+print("Correlation matrix for female abalones:\n", correlation_matrix)
+
+#infant correlation
+correlation_matrix = D_infant.drop(columns=['Sex']).corr()
+
+# Print the correlation matrix
+print("Correlation matrix for infant abalones:\n", correlation_matrix)
+
+
+
+# Male covariance
+covariance_matrix_male = D_male.drop(columns=['Sex']).cov()
+
+# Print the covariance matrix
+print("Covariance matrix for male abalones:\n", covariance_matrix_male)
+
+# Female covariance
+covariance_matrix_female = D_female.drop(columns=['Sex']).cov()
+
+# Print the covariance matrix
+print("Covariance matrix for female abalones:\n", covariance_matrix_female)
+
+# Infant covariance
+covariance_matrix_infant = D_infant.drop(columns=['Sex']).cov()
+
+# Print the covariance matrix
+print("Covariance matrix for infant abalones:\n", covariance_matrix_infant)
