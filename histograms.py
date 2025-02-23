9606726896efe84798f1bc304ab2087f918e1b74
Ro Nanak Prasad Lacoul <nanaklacoulbus@gmail.com>
2025-02-23 16:38:42 +0100

    begynder på at lave plots af forskellige typer

---
+++ histograms.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+D = pd.read_csv('MachineLearningOgDataMining/Projekt/Machlearn/abalone/abalone.data', sep=';')
+# D = pd.read_csv('MachineLearningOgDataMining/Projekt/Machlearn/abalone/abalone.data', sep=';') der hvor i har filen
+# D = pd.read_csv('MachineLearningOgDataMining/Projekt/Machlearn/abalone/abalone.data', sep=';') der hvor i har filen
+
+Male = D["Sex"]== "M"
+Female = D["Sex"] == "F"
+Infant = D["Sex"] == "I"
+
+# "Sex";"Length";"Diameter";"Height";"Whole weight";"Shucked weight";"Viscera weight";"Shell weight";"Rings"
+# parameters
+
+# Assign values based on sex
+D_male = D[Male]
+D_female = D[Female]
+D_infant = D[Infant]
+
+#Stats about abalones
+maleinfo = D_male.describe()
+femaleinfo = D_female.describe()
+infantinfo = D_infant.describe()
+datainfo = D.describe()
+
+
+
+
+# QQ plots for each parameter for abalone males
+parameters = ["Length", "Diameter", "Height", "Whole weight", "Shucked weight", "Viscera weight", "Shell weight", "Rings"]
+
+
+################################### histograms
+
+fig, axes = plt.subplots(4, 2, figsize=(15, 20))
+axes = axes.flatten()
+
+for i, param in enumerate(parameters):
+    n, bins, patches = axes[i].hist(D_male[param], bins=30, edgecolor='black', density=True)
+    axes[i].set_title(f'Histogram of {param} for male abalones')
+
+    x = np.linspace(D_male[param].min(), D_male[param].max(), 100)
+    y = stats.norm.pdf(x, np.mean(D_male[param]), np.std(D_male[param]))
+    axes[i].plot(x, y, 'r')
+
+plt.tight_layout()
+plt.show()
+
+fig, axes = plt.subplots(4, 2, figsize=(15, 20))
+axes = axes.flatten()
+
+for i, param in enumerate(parameters):
+    n, bins, patches = axes[i].hist(D_female[param], bins=30, edgecolor='black', density=True)
+    axes[i].set_title(f'Histogram of {param} for female abalones')
+
+    x = np.linspace(D_female[param].min(), D_female[param].max(), 100)
+    y = stats.norm.pdf(x, np.mean(D_female[param]), np.std(D_female[param]))
+    axes[i].plot(x, y, 'r')
+
+plt.tight_layout()
+plt.show()
+
+fig, axes = plt.subplots(4, 2, figsize=(15, 20))
+axes = axes.flatten()
+
+for i, param in enumerate(parameters):
+    n, bins, patches = axes[i].hist(D_infant[param], bins=30, edgecolor='black', density=True)
+    axes[i].set_title(f'Histogram of {param} for infant abalones')
+
+    x = np.linspace(D_infant[param].min(), D_infant[param].max(), 100)
+    y = stats.norm.pdf(x, np.mean(D_infant[param]), np.std(D_infant[param]))
+    axes[i].plot(x, y, 'r')
+
+plt.tight_layout()
+plt.show()
--- qqplots.py
+++ qqplots.py
@@ -26,6 +26,9 @@ femaleinfo = D_female.describe()
 infantinfo = D_infant.describe()
 datainfo = D.describe()
 
+
+################################### qq-plots
+
 # QQ plots for each parameter for abalone males
 parameters = ["Length", "Diameter", "Height", "Whole weight", "Shucked weight", "Viscera weight", "Shell weight", "Rings"]
 
@@ -55,6 +58,50 @@ axes = axes.flatten()
 for i, param in enumerate(parameters):
     sm.qqplot(D_infant[param], line='s', ax=axes[i])
     axes[i].set_title(f'QQ plot of {param} for infant abalones')
+
+plt.tight_layout()
+plt.show()
+
+################################### histograms
+
+fig, axes = plt.subplots(4, 2, figsize=(15, 20))
+axes = axes.flatten()
+
+for i, param in enumerate(parameters):
+    n, bins, patches = axes[i].hist(D_male[param], bins=30, edgecolor='black', density=True)
+    axes[i].set_title(f'Histogram of {param} for male abalones')
+
+    x = np.linspace(D_male[param].min(), D_male[param].max(), 100)
+    y = stats.norm.pdf(x, np.mean(D_male[param]), np.std(D_male[param]))
+    axes[i].plot(x, y, 'r')
+
+plt.tight_layout()
+plt.show()
+
+fig, axes = plt.subplots(4, 2, figsize=(15, 20))
+axes = axes.flatten()
+
+for i, param in enumerate(parameters):
+    n, bins, patches = axes[i].hist(D_female[param], bins=30, edgecolor='black', density=True)
+    axes[i].set_title(f'Histogram of {param} for female abalones')
+
+    x = np.linspace(D_female[param].min(), D_female[param].max(), 100)
+    y = stats.norm.pdf(x, np.mean(D_female[param]), np.std(D_female[param]))
+    axes[i].plot(x, y, 'r')
+
+plt.tight_layout()
+plt.show()
+
+fig, axes = plt.subplots(4, 2, figsize=(15, 20))
+axes = axes.flatten()
+
+for i, param in enumerate(parameters):
+    n, bins, patches = axes[i].hist(D_infant[param], bins=30, edgecolor='black', density=True)
+    axes[i].set_title(f'Histogram of {param} for infant abalones')
+
+    x = np.linspace(D_infant[param].min(), D_infant[param].max(), 100)
+    y = stats.norm.pdf(x, np.mean(D_infant[param]), np.std(D_infant[param]))
+    axes[i].plot(x, y, 'r')
 
 plt.tight_layout()
 plt.show()
