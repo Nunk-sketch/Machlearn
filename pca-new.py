15c8fae617515ffaaac8dcbd486bc074033b7621
ValdemarF20 <valdemarfoster.dk@gmail.com>
2025-03-06 12:57:51 +0100

    PCA Changes

--- pca-new.py
+++ pca-new.py
@@ -1,11 +1,36 @@
 from data import *
-from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from scipy import stats
 
-D_np = D_clean.to_numpy() # convert to numpy arrays
-#D_std = D_np - np.ones((N, 1)) * D_np.mean(axis=0) # mean = 0
-D_std = StandardScaler().fit_transform(D_np) # mean = 0, std = 1 (standardized)
+# Standardize the data
+D_std = (D_clean - D_clean.mean()) / D_clean.std()
 
-corr_mat = np.corrcoef(D_std.T) # correlation matrix
+X = D_std.drop(columns=["Rings"])
+Y = D_std
+X_and_Y = pd.concat([X, Y], axis=1)
+
+pca = PCA()
+pca.fit(X)
+X_pca = pca.transform(X)
+
+#%% Remove outliers
+# Calculate Z-scores
+z_scores = np.abs(stats.zscore(X_pca))
+
+# Define a threshold for Z-scores
+threshold = 5
+
+# Filter out the outliers
+outlier_mask = (z_scores < threshold).all(axis=1)
+X_pca = X_pca[outlier_mask]
+X_and_Y = X_and_Y[outlier_mask]
+
+print(f"Original shape: {(4177, 7)}")
+print(f"Filtered shape: {X_pca.shape}")
+print(f"Number of outliers: {4177 - X_pca.shape[0]}")
+
+#%% Plot the explained variance (this section could also use the PCA package, it was tested with and without and gave same results)
+corr_mat = np.corrcoef(X.T) # correlation matrix
 
 eigenvalues, eigenvectors = np.linalg.eig(corr_mat) # eigenvalues and eigenvectors
 
@@ -23,7 +48,7 @@ print(f"Variance explained: {variance_explained}")
 cumulative_variance = np.cumsum(variance_explained) # cumulative variance explained
 print(f"Cumulative variance: {cumulative_variance}")
 
-threshold = 90
+threshold = 92.5
 
 #Plot variance explained by the principal components
 plt.figure(figsize=(8, 6))
@@ -36,23 +61,27 @@ plt.xlabel('Principal components')
 plt.legend(["Individual", "Cumulative", "Threshold"])
 plt.tight_layout()
 plt.grid()
-plt.savefig('variance_explained.png')
+plt.savefig('billeder/variance_explained.png')
 plt.show()
 
-# Plot classifications
-# Use 2 components to explain more than 90% of the variance (could use 3 for 95%)
-projection_mat = np.hstack((pairs[0][1].reshape(8,1),
-                           pairs[1][1].reshape(8,1)))
-
-D_new = D_std.dot(projection_mat) # project the data onto the principal components
+#%% Plot the principal components
+i, j = 3, 4 # components to plot
 
 # Plot the PCA with rings
 plt.figure(figsize=(8, 6))
-scatter = plt.scatter(D_new[:, 0], D_new[:, 1], c=D['Rings'], cmap='viridis', edgecolor='k', s=100)
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
+scatter = plt.scatter(X_pca[:, i], X_pca[:, j], c=X_and_Y['Rings'], cmap='viridis', s=100) # edgecolor='k'
+plt.xlabel('Principal Component {}'.format(i+1))
+plt.ylabel('Principal Component {}'.format(j+1))
 plt.title('PCA of Abalone dataset')
 plt.colorbar(scatter, label='Number of Rings')
 plt.grid()
-plt.savefig('pca-graph.png')
+
+# Plot attribute coefficients in principal component space
+n_attributes = 7
+for att in range(n_attributes):
+    plt.arrow(0, 0, X_pca[att, i], X_pca[att, j], color='0', head_width=0.05, head_length=0.1, linewidth=2)
+    plt.text(X_pca[att, i], X_pca[att, j], parameters[att], color='r', fontsize=12)
+
+# Save figure and show the plot
+plt.savefig('billeder/pca-graph.png')
 plt.show()
